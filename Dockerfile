799b8ca498d5d7d1cc501fade494bea3f1b41050
gaston <C.GESMAN@tessa.com.uy>
2025-12-10 21:04:03 -0300

    downgrade prisma to version 6 to have  a stabled version

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Etapa 1: build
+FROM node:20-alpine AS builder
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+RUN npm run build
+
+# Etapa 2: runtime
+FROM node:20-alpine AS runner
+WORKDIR /app
+
+ENV NODE_ENV=production
+
+COPY package*.json ./
+RUN npm install --omit=dev
+
+COPY --from=builder /app/dist ./dist
+COPY prisma ./prisma
+
+ENV PORT=3000
+EXPOSE 3000
+
+CMD ["node", "dist/main.js"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EducationModule } from './education/education.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [UsersModule, EducationModule, PrismaModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
